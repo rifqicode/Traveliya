3ce2bb6499180202669cbb795051cd045145e9e8
Misbahul123 <baulmania@gmail.com>
2018-02-12 09:19:00 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
           <div class="row">
             <div class="col-lg-8 mx-auto">
               <h1 class="brand-heading">Traveliya</h1>
-              <p class="intro-text">Jalan-jalan kuy</p>
+              <p class="intro-text">Jalan-jalan yuk</p>
               <a href="#about" class="btn btn-circle js-scroll-trigger">
                 <i class="fa fa-angle-double-down animated"></i>
               </a>
