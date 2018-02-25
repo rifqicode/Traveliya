0cdcfa45a30199a342e53cde2c312ee1fbcace6d
rifqicode <muhammadrifqi.tb@gmail.com>
2018-02-25 21:24:15 +0700

    fix seeder

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -12,16 +12,13 @@ class RoleTableSeeder extends Seeder
     public function run()
     {
 
-      DB::table('Role')->insert([
-          'name' => 'costumer',
-          'display_name' => 'costumer',
-          'description' => 'costumer',
-      ]);
 
-      DB::table('Role')->insert([
-          'name' => 'admin',
-          'display_name' => 'admin',
-          'description' => 'admin',
-      ]);
+      $role = [
+              ['id' => '1' , 'name' => 'costumer' , 'display_name' => 'costumer' , 'description' => 'costumer'],
+              ['id' => '2' , 'name' => 'admin' , 'display_name' => 'costumer' , 'description' => 'costumer']
+
+              ];
+
+      DB::table('Roles')->insert($role);
     }
 }
--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -32,8 +32,11 @@
 													{{ csrf_field() }}
 													<input type="hidden" name="type_trip" value="Single_Trip">
 												<div class="input-field">
-													<label for="from">Dari  :</label>
-													<input type="text" name="from" class="form-control" placeholder="Kota/Bandara"/>
+													<!-- <label for="from">Dari  :</label>
+													<input type="text" name="from" class="form-control" placeholder="Kota/Bandara"/> -->
+													<select class="select2 item-id form-control" name="from">
+																<option value="1">asdasd</option>
+													</select>
 												</div>
 											</div>
 											<div class="col-xxs-12 col-xs-6 mt">
